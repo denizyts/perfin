56a18bff898ee213b8015e8f81f2b6d86acea49e
AlpBost <alpbost1@gmail.com>
2024-12-01 15:43:45 +0300

    last commit

--- src/myGui.java
+++ src/myGui.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class myGui extends JFrame {
+class myGui extends JFrame {
 
     public myGui() {
         setTitle("Stock Dashboard");
@@ -42,7 +42,7 @@ public class myGui extends JFrame {
             }
         });
 
-        //leftPanel.add(Box.createRigidArea(new Dimension(0, 20))); // Dropdown ile diğer içerikler arasında boşluk
+        leftPanel.add(Box.createRigidArea(new Dimension(0, 20))); 
     
         leftPanel.add(Box.createRigidArea(new Dimension(0, 200))); // BIST 100 konumu 
     
@@ -65,69 +65,64 @@ public class myGui extends JFrame {
         centerPanel.setBackground(new Color(35, 39, 64));
         centerPanel.setBounds(340, 20, 300, 520);
         centerPanel.setLayout(new BorderLayout());
-    
+        
         JLabel portfolioLabel = new JLabel("PORTFOLIO", JLabel.CENTER);
         portfolioLabel.setForeground(Color.WHITE);
         portfolioLabel.setFont(new Font("Arial", Font.BOLD, 18));
         centerPanel.add(portfolioLabel, BorderLayout.NORTH);
-    
+        
         JPanel pieChartPanel = new JPanel() {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
                 g.setColor(Color.ORANGE);
-                g.fillArc(50, 30, 200, 200, 0, 180); // THYAO: 50%
+                g.fillArc(50, 30, 200, 200, 0, 180); 
                 g.setColor(Color.GREEN);
-                g.fillArc(50, 30, 200, 200, 180, 72); // NVDA: 20%
+                g.fillArc(50, 30, 200, 200, 180, 72);
                 g.setColor(Color.CYAN);
-                g.fillArc(50, 30, 200, 200, 252, 108); // GOLD: 10%
+                g.fillArc(50, 30, 200, 200, 252, 108); 
             }
         };
         pieChartPanel.setPreferredSize(new Dimension(300, 200));
         pieChartPanel.setBackground(new Color(35, 39, 64));
         centerPanel.add(pieChartPanel, BorderLayout.CENTER);
-    
+
+        JPanel lowerPanel = new JPanel();
+        lowerPanel.setLayout(new BoxLayout(lowerPanel, BoxLayout.Y_AXIS));
+        lowerPanel.setBackground(new Color(35, 39, 64));
         
+        JLabel totalPriceLabel = new JLabel("Total Budget", JLabel.CENTER);
+        totalPriceLabel.setForeground(Color.WHITE);
+        totalPriceLabel.setFont(new Font("Arial", Font.BOLD, 18));
+        totalPriceLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        lowerPanel.add(totalPriceLabel);
         
-        JPanel operationPanel = new JPanel();
-        operationPanel.setBackground(new Color(35, 39, 64));
-        operationPanel.setLayout(new BoxLayout(operationPanel, BoxLayout.Y_AXIS));
-    
-        JLabel operationLabel = new JLabel("        ---Last 5 Operations---", JLabel.CENTER);
+        JLabel operationLabel = new JLabel("---Last 5 Operations---", JLabel.CENTER);
         operationLabel.setForeground(Color.WHITE);
         operationLabel.setFont(new Font("Arial", Font.BOLD, 18));
-        operationPanel.add(operationLabel);
+        operationLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        lowerPanel.add(operationLabel);
         
-    
-        // Son 5 işlemi simüle etme
         for (int i = 1; i <= 5; i++) {
-            JLabel operationItem = new JLabel( i + ") TUCLK(BUY) - 20 Shares - 130,30   ", JLabel.CENTER);
+            JLabel operationItem = new JLabel(i + ") TUCLK(BUY) - 20 Shares - 130,30", JLabel.CENTER);
             operationItem.setForeground(Color.WHITE);
             operationItem.setFont(new Font("Arial", Font.PLAIN, 14));
-            operationPanel.add(operationItem);
+            operationItem.setAlignmentX(Component.CENTER_ALIGNMENT);
+            lowerPanel.add(operationItem);
         }
-    
-        centerPanel.add(operationPanel, BorderLayout.SOUTH);
-    
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.setBackground(new Color(35, 39, 64)); 
-        buttonPanel.setForeground(new Color(0, 31, 63));
-
+        
         JButton openNewPageButton = new JButton("More Operations");
-        openNewPageButton.setPreferredSize(new Dimension(75, 25)); 
+        openNewPageButton.setPreferredSize(new Dimension(150, 30));
         openNewPageButton.addActionListener(e -> openNewPage());
-        buttonPanel.add(openNewPageButton);
+        openNewPageButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        lowerPanel.add(Box.createRigidArea(new Dimension(0, 10))); // Buton üstü boşluk
+        lowerPanel.add(openNewPageButton);
+        
+        centerPanel.add(lowerPanel, BorderLayout.SOUTH);
     
-        centerPanel.add(buttonPanel, BorderLayout.SOUTH); 
-    
-        JPanel mainOperationPanel = new JPanel();
-        mainOperationPanel.setLayout(new BorderLayout());
-        mainOperationPanel.add(operationPanel, BorderLayout.NORTH);
-        mainOperationPanel.add(buttonPanel, BorderLayout.SOUTH);
-        centerPanel.add(mainOperationPanel, BorderLayout.SOUTH);
-
         return centerPanel;
     }
+    
 
     private void openNewPage() {
         new OperationPage(); 
@@ -172,6 +167,9 @@ class OperationPage extends JFrame {
         JPanel panel = createCenterPanel();
         add(panel);
 
+        JPanel panel2 = createLeftPanel();
+        add(panel2);
+
         setLocationRelativeTo(null);
         setVisible(true);
     }
@@ -187,6 +185,7 @@ class OperationPage extends JFrame {
         portfolioLabel.setForeground(Color.WHITE);
         portfolioLabel.setFont(new Font("Arial", Font.BOLD, 18));
         centerPanel.add(portfolioLabel, BorderLayout.NORTH);
+    
         
         // İşlemler paneli
         JPanel showOperationPanel = new JPanel();
@@ -216,6 +215,26 @@ class OperationPage extends JFrame {
         operationItem.setCaretPosition(0); //scroll bunu koymazsan sonda kalıyor 
 
         return centerPanel;
+    }
+
+    private JPanel createLeftPanel () {
+        JPanel leftPanel = new JPanel();
+        leftPanel.setBackground(new Color(35, 39, 64));
+        leftPanel.setBounds(20, 20, 250, 320);
+        leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
+
+        JButton backButton = new JButton("Back");
+        backButton.setPreferredSize(new Dimension(75, 25)); 
+        backButton.addActionListener(e -> homePage());
+        leftPanel.add(backButton);
+
+
+        return leftPanel;
+    }
+
+    private void homePage() {
+        new myGui();
+        this.dispose();
     }
     
     
--- src/tempCodeRunnerFile.java
+++ src/tempCodeRunnerFile.java
@@ -1,1 +1,2 @@
-backBu
+
+        // ekleme label 
